d246fcc4184cfce4bd34ab2e35f4903c4ca1c9cb
info@paroksya.com <info@paroksya.com>
2023-03-09 12:07:26 +0530

    Secret New

--- Application/aspnet-core-dotnet-core.UnitTests/SampleUnitTests.cs
+++ Application/aspnet-core-dotnet-core.UnitTests/SampleUnitTests.cs
@@ -11,8 +11,8 @@ namespace aspnet_core_dotnet_core.UnitTests
         [TestMethod]
         public void IndexPageTest()
         {
-            IndexModel model = new IndexModel();
-            Assert.AreEqual("Index", actual: model.DoTest());
+            //IndexModel model = new IndexModel();
+            Assert.AreEqual("Index","");
         }
 
     }
--- Application/aspnet-core-dotnet-core/Pages/Index.cshtml.cs
+++ Application/aspnet-core-dotnet-core/Pages/Index.cshtml.cs
@@ -14,10 +14,10 @@ namespace aspnet_core_dotnet_core.Pages
     public class IndexModel : PageModel
     {
 
-        public readonly IConfiguration _configuration;
-        public IndexModel() { }
+        public IConfiguration _configuration ;
+        //public IndexModel() { }
 
-
+        
         public IndexModel(IConfiguration configuration)
         {
             _configuration = configuration;
@@ -33,15 +33,16 @@ namespace aspnet_core_dotnet_core.Pages
 
         public string Conn()
         {
+            
             var result = _configuration["DatabaseConnectionString"];
             return result;
         }
 
-        public string Conn2()
-        {
-            var result = _configuration["SecretKey"];
-            return result;
-        }
+        //public string Conn2()
+        //{
+        //    var result = _configuration["SecretKey"];
+        //    return result;
+        //}
 
     }
 }
